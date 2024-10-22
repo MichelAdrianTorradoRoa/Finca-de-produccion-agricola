6ad38d49fd045830c3cdbfa4daea913ad5120159
Juan David Conde Martinez <juanconde1309@gmail.com>
2024-10-22 14:49:50 -0500

    fix: :bug: Correccion trigger
    By @juanconde025

--- DQL/dql. triggers.sql
+++ DQL/dql. triggers.sql
@@ -112,7 +112,7 @@ DELIMITER ;
 -- Verificar inventario suficiente para una venta
 DELIMITER //
 CREATE TRIGGER verificar_inventario_antes_venta
-BEFORE INSERT ON ventas
+BEFORE INSERT ON productos_ventas
 FOR EACH ROW
 BEGIN
 DECLARE cantidad_disponible INT;
@@ -124,7 +124,6 @@ IF cantidad_disponible < 1 THEN
 SIGNAL SQLSTATE '45000'
 SET MESSAGE_TEXT = 'inventario insuficiente para realizar la venta';
 END IF;
-    
 END //
 DELIMITER ;
 
